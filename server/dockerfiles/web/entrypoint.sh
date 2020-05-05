a35a44b1ebef888912b8198a371e772941396e4e
alireza <sdamoosavi@gmail.com>
2020-05-05 05:33:11 +0430

    kafka docker up good!

--- server/dockerfiles/web/entrypoint.sh
+++ server/dockerfiles/web/entrypoint.sh
@@ -16,4 +16,12 @@ then
     echo "PostgreSQL started"
 fi
 
+
+set -e
+
+until nc -vz ${KAFKA_SERVER_SERVER_NAME} ${KAFKA_SERVER_SERVER_PORT}; do
+  >&2 echo "Waiting for Kafka to be ready... - sleeping"
+  sleep 2
+done
+
 exec "$@"
